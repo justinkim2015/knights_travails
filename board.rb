d05c15d6346ce2bdfc913520ecbedd56ae29f2ec
justinkim2015 <justinkim2015@gmail.com>
2022-05-27 12:13:54 +0900

    Write filter method for node

--- board.rb
+++ board.rb
@@ -1,5 +1,11 @@
 class Board
+  attr_accessor :nodes
+
   def initialize
     @nodes = {}
+  end
+
+  def add_node(node)
+    @nodes[node.value] = node
   end
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require_relative 'node'
 require_relative 'knight'
 require_relative 'board'
+require 'pry'
 
-board = Board.new
-board.make_board
+node = Node.new([6, 6])
+node1 = Node.new([0, 1])
+node.make_adjacent_nodes
